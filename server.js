80fa6481b95c54a0ecd1aef4f726b8a499978ebe
Jarold Wong <jaroldw@gmail.com>
2017-06-20 21:57:59 -0700

    setup socket.io

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const server = app.listen(3000, function() {
+  console.log('Server running on port 3000');
+})
+
+app.use(express.static('public'));
+
+const io = socket(server);
+
+io.on('connection', (socket => {
+  console.log('made socket connection', socket.id);
+}))
